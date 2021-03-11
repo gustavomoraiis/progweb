aa1465f840fdfd920301636b3fda299c0a260fa7
Gustavo Morais <gustavohenriquecfal@gmail.com>
2021-03-11 20:28:27 -0300

    models user

---
+++ models/usuario.js
@@ -0,0 +1,36 @@
+	
+module.exports = function(sequelize, DataTypes) {
+    const Usuario = sequelize.define(
+      'usuario',
+      {
+        id: {
+          type: DataTypes.INTEGER,
+          allowNull: false,
+          primaryKey: true,
+          autoIncrement: true
+        },
+        nome: {
+          type: DataTypes.STRING,
+          allowNull: false
+        },
+        email: {
+          type: DataTypes.STRING,
+          allowNull: false
+        },
+        senha: {
+          type: DataTypes.STRING,
+          allowNull: false
+        },
+        avatar: {
+          type: DataTypes.STRING,
+          allowNull: true
+        }
+      },
+      {
+        tableName: 'usuario',
+        timestamps: false
+      }
+    );
+    
+    return Usuario;
+  };
