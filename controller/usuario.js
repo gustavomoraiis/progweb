3e6bff690648d2f3af600a33144d5bc6e003a721
Gustavo Morais <gustavohenriquecfal@gmail.com>
2021-03-12 21:54:18 -0300

    controller

--- controller/usuario.js
+++ controller/usuario.js
@@ -1,15 +1,31 @@
-const database = require('../models');
+const { Usuario } = require('../models');
 const controller = {};
 
-// controller.getUsuario() = () => {
-//     retuner [
-//         {
-//             "nome" : "Gustavo"
-//         },
-//         {
-//             "nome" : "Morais"
-//         }
-//     ];
-// };
+controller.getUsuario = async (id = null) => {
+    let result = [];
+    if (id){
+       result = await Usuario.fundByPk(id);
+    } else {
+       result = await Usuario.fundAll();
+    }
+
+    return result;
+};
+
+controller.save = async (usuario) => {
+    return await Usuario.create(usuario);
+};
  
+controller.edit = async (id, usuario) => {
+    await Usuario.update(usuario, {
+        where: {id},
+    });
+    return await controller.getUsuario(id);
+};
+
+controller.remove = async (id) =>{
+    
+    return await Usuario.destroy({where: {id}});
+};
+
 module.exports = controller;
